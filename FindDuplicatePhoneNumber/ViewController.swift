dabe07629bc4f084230039b8a04c1671ddef3a64
de4life <ageev_artur@ukr.net>
2022-10-05 16:36:01 +0300

    add cells

--- FindDuplicatePhoneNumber/ViewController.swift
+++ FindDuplicatePhoneNumber/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblHeader: UILabel!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let names = [
+        "Jon",
+        "Dick",
+        "Rich",
+        "Duck",
+        "Smooya"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped me")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
+}
